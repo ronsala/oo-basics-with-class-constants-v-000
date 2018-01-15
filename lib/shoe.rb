b3bc17aae7d7e88a3561dd6ac054833ec703de6f
Ron Sala <ron.sala@gmail.com>
2018-01-15 07:20:15 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
-    else
-      BRANDS << brand
-    end
+    BRANDS.include?(brand) ? : BRANDS << brand
   end
 
   def cobble
